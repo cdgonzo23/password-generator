3d4d9fdc339125ecff4f9a784dedda1fb9cc14e9
Christian Gonzales <124095890+cdgonzo23@users.noreply.github.com>
2023-05-04 22:13:42 -0500

    updated format

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ function generatePassword() {
   var numbersArr = numbers.split('');
   var specialCharacters = "!#$%&'()*+,./:;<=>?@[\]^_`{|}~";
   var specialCharArr = specialCharacters.split('');
-
   var lengthResult = parseInt(prompt("How many characters will this password have? (Must be between 8 and 128 characters)"));
 
   if (!lengthResult || lengthResult < 8 || lengthResult > 128) {
@@ -21,46 +20,39 @@ function generatePassword() {
   }
 
   var specialResult = confirm("Click OK to confirm including special characters.");
-
   var numbersResult = confirm("Click OK to confirm including numeric characters.");
-
   var lowercaseResult = confirm("Click OK to confirm including lowercase characters.");
-
   var uppercaseResult = confirm("Click OK to confirm including uppercase characters.");
-
   var stored = [];
-
   var characterResult = [];
 
   if (specialResult) {
     characterResult = characterResult.concat(specialCharArr);
     var randomCharacter = specialCharArr[Math.floor(Math.random() * specialCharArr.length)]
-    stored = specialCharArr[randomCharacter]
+    stored = randomCharacter.concat(passwordResult)
   }
   if (numbersResult) {
     characterResult = characterResult.concat(numbersArr);
-    var randomCharacter = specialCharArr[Math.floor(Math.random() * specialCharArr.length)]
-    stored = specialCharArr[randomCharacter]
+    var randomCharacter = numbersArr[Math.floor(Math.random() * numbersArr.length)]
+    stored = randomCharacter.concat(passwordResult)
   }
   if (lowercaseResult) {
     characterResult = characterResult.concat(lowercaseArr);
-    var randomCharacter = specialCharArr[Math.floor(Math.random() * specialCharArr.length)]
-    stored = specialCharArr[randomCharacter]
+    var randomCharacter = lowercaseArr[Math.floor(Math.random() * lowercaseArr.length)]
+    stored = randomCharacter.concat(passwordResult)
   }
   if (uppercaseResult) {
     characterResult = characterResult.concat(uppercaseArr);
-    var randomCharacter = specialCharArr[Math.floor(Math.random() * specialCharArr.length)]
-    stored = specialCharArr[randomCharacter]
+    var randomCharacter = uppercaseArr[Math.floor(Math.random() * uppercaseArr.length)]
+    stored = randomCharacter.concat(passwordResult)
   }
 
   if (!specialResult && !numbersResult && !lowercaseResult && !uppercaseResult) {
-     alert("please choose at least 1 character type for your password");
+    alert("please choose at least 1 character type for your password");
     return passwordResult;
   }
   
-
   for (var i = 0; i < lengthResult; i++) {
-    // passwordResult += characterResult.charAt(Math.floor(Math.random() * characterResult.length));
     var randomCharacter = characterResult[Math.floor(Math.random() * characterResult.length)]
     passwordResult.push(randomCharacter)
   };
